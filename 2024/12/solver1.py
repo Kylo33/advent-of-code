df6ee19bca7e65531c10c2f2955d9783b6d549a6
Kylo33 <56988649+Kylo33@users.noreply.github.com>
2024-12-12 13:11:43 -0700

    Add optimization for Part 1 Dec 12, 2024

--- 2024/12/solver1.py
+++ 2024/12/solver1.py
@@ -3,7 +3,7 @@ from itertools import chain
 DELTAS = ((1, 0), (-1, 0), (0, 1), (0, -1))
 
 def main():
-    with open("test_input") as f:
+    with open("input") as f:
         farm = [list(line) for line in f.read().strip().split("\n")]
         regions = get_regions(farm)
 
@@ -26,16 +26,22 @@ def get_regions(farm):
     for y, line in enumerate(farm):
         for x, plant in enumerate(line):
             if (x, y,) not in visited:
-                regions.append(traverse(x, y, farm, visited))
+                region = set()
+                traverse(x, y, farm, visited, region)
+                regions.append(region)
     return regions
 
 
-def traverse(x, y, farm, visited):
+def traverse(x, y, farm, visited, region):
     if (x, y) in visited:
-        return set()
+        return
     visited.add((x, y))
-    neighbors = [(x + dx, y + dy) for dx, dy in DELTAS if in_farm(farm, x, y, dx, dy)]
-    return {(x, y)} | set(chain.from_iterable([traverse(nx, ny, farm, visited) for nx, ny in neighbors if farm[y][x] == farm[ny][nx]]))
+    region.add((x, y))
+
+    neighbors = ((x + dx, y + dy) for dx, dy in DELTAS if in_farm(farm, x, y, dx, dy))
+    for nx, ny in neighbors:
+        if farm[y][x] == farm[ny][nx]:
+            traverse(nx, ny, farm, visited, region)
 
 
 def in_farm(farm, x, y, dx, dy):
