ed002acd2f8211c3dd3ee4e00e093414454b487d
Kylo33 <56988649+Kylo33@users.noreply.github.com>
2024-12-16 13:04:45 -0700

    Solve part 2 of Dec 16, 2024

--- 2024/16/solver.py
+++ 2024/16/solver.py
@@ -1,5 +1,5 @@
 import heapq
-from enum import Enum
+import json
 
 DELTAS = {
     "N": (0, -1),
@@ -8,16 +8,19 @@ DELTAS = {
     "W": (-1, 0)
 }
 
+BIG_NUMBER = 1000000000000
+
 
 def main():
     with open("input") as f:
         maze = f.read().strip().split("\n")
-        start = find_char("S", maze), "E"
+        start = find_char("S", maze)
         end = find_char("E", maze)
 
-    p1 = part1(maze, start)
-    print(f"Part 1: {p1}")
-    print(f"Part 2: {part2(maze, start, p1)}")
+    p1_ans = part1(maze, start, end, "E")
+    p2_ans = part2(maze, start, end, "E", p1_ans)
+    print(f"Part 1: {p1_ans}")
+    print(f"Part 2: {p2_ans}")
 
 
 def find_char(char, maze):
@@ -30,22 +33,24 @@ def get_new_directions(direction):
     return ds[(idx - 1) % 4], ds[(idx + 1) % 4]
 
 
-def part1(maze, start):
+def shortest_paths(maze, start, dxn, to=None):
+    costs = [[(-1, "") for _ in range(len(maze[0]))] for _ in range(len(maze))]
     priority_queue = []
-    reindeer, dxn = start
+    x, y = start
+    costs[y][x] = (0, dxn)
+    dx, dy = DELTAS[dxn]
 
-    direction = DELTAS[dxn]
-    straight_ahead = (reindeer[0] + direction[0], reindeer[1] + direction[1])
+    straight_ahead = (x + dx, y + dy)
     if not maze[straight_ahead[1]][straight_ahead[0]] == '#':
         heapq.heappush(priority_queue, (1, (straight_ahead, dxn)))
 
     for new_dxn in get_new_directions(dxn):
-        new_direction = DELTAS[new_dxn]
-        ahead = (reindeer[0] + new_direction[0], reindeer[1] + new_direction[1])
-        if not maze[ahead[1]][ahead[0]] == '#':
-            heapq.heappush(priority_queue, (1001, (ahead, new_dxn)))
+        new_dx, new_dy = DELTAS[new_dxn]
+        new_x, new_y = x + new_dx, y + new_dy
+        if not maze[new_y][new_x] == '#':
+            heapq.heappush(priority_queue, (1001, ((new_x, new_y), new_dxn)))
 
-    seen = {reindeer}
+    seen = {(x, y)}
     while len(priority_queue) > 0:
         cost, pos_info = heapq.heappop(priority_queue)
         node, dxn = pos_info
@@ -53,7 +58,9 @@ def part1(maze, start):
             continue
         seen.add(node)
         x, y = node
-        if maze[y][x] == "E":
+        costs[y][x] = cost, dxn
+
+        if (x, y) == to:
             return cost
 
         possible_next_nodes = []
@@ -67,16 +74,29 @@ def part1(maze, start):
         for next_node in filter(lambda n: maze[n[1][0][1]][n[1][0][0]] != '#', possible_next_nodes):
             heapq.heappush(priority_queue, next_node)
 
-    return -1
+    if to is not None:
+        return BIG_NUMBER
+    
+    return costs
 
 
+def part1(maze, start, end, starting_dxn):
+    return shortest_paths(maze, start, starting_dxn, to=end)
 
-def part2(maze, start, p1):
-    tiles = set()
-    reindeer, dxn = start
+def part2(maze, start, end, starting_dxn, p1_ans):
+    costs = shortest_paths(maze, start, starting_dxn)
 
-    return len(tiles)
-
+    through_best_path = {start, end}
+    for y, line in enumerate(costs):
+        for x, cost_info in enumerate(line):
+            cost, starting_dxn = cost_info
+            if cost == -1 or (x, y) == end:
+                continue
+            if cost + shortest_paths(maze, (x, y), starting_dxn, to=end) == p1_ans:
+                through_best_path.add((x, y))
+    
+    return len(through_best_path)
+            
 
 if __name__ == "__main__":
     main()
